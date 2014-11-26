af839cf45200609213541a1f87c4dd31e6b3a557
Andreas Weber <code@andreas-weber.me>
2014-11-26 16:35:00 +0100

    bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,12 @@ This is the list of changes for the Version 1.0 release series.
 ## Version 1.0.0
 
 * Initial release.
+
+## Version 1.0.1
+
+* Small improvements
+* Bump fdomdocument version to 1.6.0
+
+## Version 1.0.2
+
+* Bump library version
--- src/Console/Application.php
+++ src/Console/Application.php
@@ -1,21 +1,21 @@
 <?php
 /**
  * phpjunitmerge
- * 
+ *
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2014, Andreas Weber <code@andreas-weber.me>.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -56,7 +56,7 @@ class Application extends ConsoleApplication
      */
     public function __construct()
     {
-        parent::__construct('phpjunitmerge', '1.0.0');
+        parent::__construct('phpjunitmerge', '1.0.1');
     }
 
 
@@ -64,7 +64,7 @@ class Application extends ConsoleApplication
      * Gets the name of the command based on input.
      *
      * @param InputInterface $input
-     * 
+     *
      * @return string The command name
      */
     protected function getCommandName(InputInterface $input)
